ab34ca80ada406e11c64fa34f238e77335a19c5a
Unknown <i.s.rusakov@gmail.com>
2017-05-10 11:56:15 +0700

    Upload IR codes

---
+++ Digispark ir codes/LG.java
@@ -0,0 +1,52 @@
+Ïףכüע LG
+On 16
+Subtitle 156
+ratio 158
+input 208
+tv/rad 15
+1 136
+2 72
+3 200
+4 40
+5 168
+6 104
+7 232
+8 24
+9 152
+0 8
+list 202
+q.view 88
+vol up 64
+vol down 192
+fav 120
+guide 213
+mute 144
+ch up 0
+ch down 128
+info 85
+smart 62
+my apps 66
+left 224
+right 96
+up 2
+down 130
+OK 34
+back 20
+settings 194
+exit 218
+red 78
+green 142
+yellow 198
+blue 134
+text 4
+t.opt 132
+q.menu 162
+stop 141
+play 13
+pause 93
+rew 241
+fwd 113
+rec 189
+sleep 112
+AD 137
+app 249
---
+++ Digispark ir codes/pears.java
@@ -0,0 +1,43 @@
+Ïףכüע Ïטנס
+On 194
+mute 226
+Rec 242
+Prev 216
+Next 200
+Pause 42
+Info 10
+Menu 120
+Up 114
+Down 122
+Left 16
+Right 32
+OK 168
+Home 68
+Zoom 248
+Return 72
+Vol up 136
+Vol down 184
+audio 88
+subt 152
+ch up 40
+ch down 104
+red 186
+green 154
+yellow 224
+blue 138
+rotate 210
+rew 56
+fwd 8
+PIP 74
+1 66
+2 130
+3 2
+4 98
+5 162
+6 34
+7 82
+8 146
+9 18
+0 178
+del 250
+F 90
